e3f6281cfe85ae20756324d88aa97a11108707d8
navyaneethi <navyaneethis@gmail.com>
2026-01-27 20:55:58 +0530

    triviabot

---
+++ GreetingBot/triviabot.js
@@ -0,0 +1,15 @@
+console.log("Hello! I'm your coding fun fact guide!");
+let botName = "Jimmy", botLocation = "US", favoriteLanguage = "JavaScript";
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+
+console.log("My favorite programming language is "+favoriteLanguage+".");
+
+let codingFact = "Did you know that "+ favoriteLanguage + " was created in just 10 days?";
+console.log(codingFact);
+
+codingFact = "Also,"+ favoriteLanguage + "is the most popular programming language in the world!";
+console.log(codingFact);
+
+codingFact = "fun fact: "+ favoriteLanguage + " can be used for both front-end and back-end development.";
+console.log(codingFact);
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
