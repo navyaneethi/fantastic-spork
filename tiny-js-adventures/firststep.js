c4aa4da98ba0f2cbb119f9c714c886fa0c034ce2
navyaneethi <navyaneethis@gmail.com>
2026-01-27 16:49:04 +0530

    Initial js lessons

---
+++ tiny-js-adventures/firststep.js
@@ -0,0 +1,65 @@
+console.log("Hello World from FirstSteps.js!");
+
+//variables
+let age =25
+console.log(age)
+
+let $total = 0
+
+//const salary = 0
+//salary =1000
+//console.log(salary)
+
+let sum =0
+sum = 5
+console.log(sum)
+
+const anme = 'Vishwas'  //string
+const language = "JavaScript"
+const channel = `Codevolution`
+
+const total = 0
+const PI = 3.14 //number
+
+const isPrimaryNumner = true
+const isNewUser = false //boolean
+
+let result
+console.log(result) //undefined
+
+const res = undefined
+const data =null  //null
+
+const person ={
+    'firstName':'Bruce',
+    'lastName':'Wayne',
+    age:30
+}
+
+console.log(person)
+
+console.log(person.firstName)
+console.log(person['lastName'])
+
+const oddNumbers = [1,3,5,7,9]
+console.log(oddNumbers[1])
+
+let a =10
+a = 'Vishwas'
+a = true
+console.log(a)
+
+let developer = "Jessica";
+console.log(developer);
+
+developer = "JessicA";
+console.log(developer);
+
+let str1 = 'con'
+let str2 = 'Cat'
+
+let greeting = str1 + ' ' + str2
+console.log(greeting)
+
+greeting = str1.concat(' ', str2)
+console.log(greeting)
