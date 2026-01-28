98467c8b55286b44c4eb278a98899ee3eea7c1fd
navyaneethi <navyaneethis@gmail.com>
2026-01-28 16:56:23 +0530

    Sentence maker

---
+++ GreetingBot/sentenceMaker.js
@@ -0,0 +1,33 @@
+let adjective = "friendly";
+let noun = "dragon";
+let noun2 = "marshmallows";
+let place = "mystical cave";
+let adjective2 = "fiery";
+let verb = "angry";
+
+let firstStory =
+  "Once upon a time, there was a(n) " + adjective + " " + noun +
+  " who loved to eat " + noun2 +
+  ". The " + noun +
+  " lived in a " + place +
+  " and had " + adjective2 +
+  " nostrils that blew fire when it was " + verb + ".";
+console.log("First story: " + firstStory);
+
+adjective="curious"
+noun="robot"
+noun2="scrap metals"
+place="busy junkyard"
+adjective2="whirring"
+verb = "overloaded"
+
+
+let secondStory = 
+"Once upon a time, there was a(n) " + adjective + " " + noun + 
+" who loved to eat " + noun2 + 
+". The "+noun+ 
+" lived in a " + place+ 
+" and had "+ adjective2 +
+ " nostrils that blew fire when it was " + verb + ".";
+
+console.log("Second story: " + secondStory);
