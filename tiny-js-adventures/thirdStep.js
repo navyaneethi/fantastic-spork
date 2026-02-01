49efe1f532711155c1c17c55835e187da45b9903
navyaneethi <navyaneethis@gmail.com>
2026-02-01 19:47:18 +0530

    freeCodeCamp workshop

--- tiny-js-adventures/thirdStep.js
+++ tiny-js-adventures/thirdStep.js
@@ -7,3 +7,8 @@ const var3 = 10;
 const var4 = '10';
 console.log(var3 == var4); // true
 console.log(var3 === var4);
+
+const var5 = null;
+const var6 = undefined;
+console.log(var5 == var6); //true
+console.log(var5 === var6);//false
